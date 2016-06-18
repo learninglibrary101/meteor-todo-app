200446622c342c7013553d225a6c4fd5bb49e57c
learninglibrary101 <learninglibrary101@users.noreply.github.com>
2016-06-18 18:55:37 +0800

    Added 7_Temporary_ui_state

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,8 +6,21 @@ import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      hideCompleted: false
+    };
+  }
+
   renderTasks(){
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if(this.state.hideCompleted){
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -27,11 +40,21 @@ class App extends Component{
     ReactDOM.findDOMNode(this.refs.textInput).value = "";
   }
 
+  toggleHideCompleted(){
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
+    });
+  }
+
   render(){
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <label className="hide-completed">
+            <input type="checkbox" readOnly checked={this.state.hideCompleted} onClick={this.toggleHideCompleted.bind(this)} />
+            Hide Completed Tasks
+          </label>
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
             <input type="text" ref="textInput" placeholder="Type to add new tasks" />
           </form>
@@ -44,11 +67,13 @@ class App extends Component{
 }
 
 App.propTypes = {
-  tasks: PropTypes.array.isRequired
+  tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired
 };
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
+    tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch(),
+    incompleteCount: Tasks.find({checked: {$ne: true}}).count()
   };
 }, App);
