2760471da406d7fd5ff23ba16a1907350c038b3e
learninglibrary101 <learninglibrary101@users.noreply.github.com>
2016-06-18 18:08:39 +0800

    Added 4_Forms_and_events

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-## 3_Collections
-
-```javascript
-meteor npm install --save react-addons-pure-render-mixin
-meteor add react-meteor-data
-```
-
-#### Additional Steps in Meteor Mongo:
-```javascript
-db.tasks.insert({ text: "Hello world!", createdAt: new Date() });
-```
+## 4_Forms_and_events
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
@@ -11,11 +12,29 @@ class App extends Component{
     ));
   }
 
+  handleSubmit(event){
+    event.preventDefault();
+
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    // Insert to DB
+    Tasks.insert({
+      text,
+      createdAt: new Date()
+    });
+
+    // clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = "";
+  }
+
   render(){
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+            <input type="text" ref="textInput" placeholder="Type to add new tasks" />
+          </form>
         </header>
 
         <ul>{this.renderTasks()}</ul>
@@ -30,6 +49,6 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch()
+    tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
   };
 }, App);
